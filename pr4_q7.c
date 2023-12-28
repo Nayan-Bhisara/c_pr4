bf8025eb2c22189fe6c1d23c6ef2e00eba8a40bb
Nayan-Bhisara <149070982+Nayan-Bhisara@users.noreply.github.com>
2023-12-28 09:00:08 +0530

    Update pr4_q7.c

--- pr4_q7.c
+++ pr4_q7.c
@@ -1,26 +1,34 @@
 /*
 Q.7 Write a Program to print the below pattern using nested for loop.
-            *
-        *     *
-      *         *
-    *             *
-  *                 *
-*/
-
-#include<stdio.h>
-
-main(){
+      *
+     * *
+    *   *
+   *     *
+  *       *
+  
+  */
+  
+  void main(){
 	
-	int i,j;
-	int k;
-	
-	for(i=5;i>=1;i--){
-		for(k=i;k<=5;k++){
+	int i,j,s;
+
+	for(i=1; i<=5; i++){
+		for(s=4; s>=i; s--){
 			printf(" ");
 		}
-		for(j=1;j<=i;j++){
-			printf("%d",j%2);
+		for(j=1; j<=1; j++){
+	
+		 	printf("* ");
+		 	
 		}
+		for(s=i-1; s>=j; s--){
+			printf("  ");
+		}
+		for(j=1; j<=s; j++){
+	
+		 	printf("*");	
+		}	
 		printf("\n");
 	}
+
 }
